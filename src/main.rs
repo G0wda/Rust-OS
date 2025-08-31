de507e633c718a94013560f53a855f3929d4f31c
Your Name <you@example.com>
2025-08-31 22:37:31 +0530

    added vga_buffer mod

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -11,40 +12,9 @@ fn panic(_info: &PanicInfo) -> ! {
 
 
 
-static HELLO: &[u8] = b"Hello world";
-static MESSAGE: &[u8] = b"Welcome to RUST OS";
-static MESSAGE_2: &[u8] = b"This is a Test Kernel written in Rust Language";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    
-    // Write HELLO
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    let row_length = 80; // VGA width
-    let mut offset = row_length; // start of 2nd line
-
-    for (i, &byte) in MESSAGE.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset((offset + i) as isize * 2) = byte;
-            *vga_buffer.offset((offset + i) as isize * 2 + 1) = 0xb;
-        }
-    }
-    offset = 160;
-
-    for ( i, &byte) in  MESSAGE_2.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset((offset + i) as isize * 2) = byte;
-            *vga_buffer.offset((offset + i) as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga_buffer::print_something();
     loop {}
 }
